8d5c2cf2d29a4595decac87bd054ace27d6ccb29
ganeshmanal <ganesh.manal@maplelabs.com>
2021-08-19 00:57:00 +0530

    fixes

--- Terraform/hadoop-single-node/compute.tf
+++ Terraform/hadoop-single-node/compute.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region           = "us-east-2"  
+  region = "us-east-2"
 }
 
 resource "aws_instance" "hadoop_namenode" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  key_name = var.aws_keypair
-  subnet_id = var.subnet_id
+  ami             = var.aws_ami
+  instance_type   = var.aws_instance_type
+  key_name        = var.aws_keypair
+  subnet_id       = var.subnet_id
   security_groups = var.security_group_ids
 
   tags = {
-    Name = "hadoop-namenode"
+    Name  = "hadoop-namenode"
     Owner = "gmanal"
   }
 
--- Terraform/hadoop-single-node/variable.tf
+++ Terraform/hadoop-single-node/variable.tf
@@ -1,14 +1,23 @@
 variable "aws_keypair" {
-    default = "DevOps-SRE"
+  default = "DevOps-SRE"
 }
 
 variable "vpc_id" {
-    default = "vpc-9ad309f1"
+  default = "vpc-9ad309f1"
 }
 variable "subnet_id" {
-    default = "subnet-d7b981ad"
+  default = "subnet-d7b981ad"
 }
 
 variable "security_group_ids" {
-    default = [ "sg-06175e42b45d69a98"] 
+  default = ["sg-06175e42b45d69a98"]
+}
+
+variable "aws_ami" {
+  default = "ami-0c55b159cbfafe1f0"
+
+}
+
+variable "aws_instance_type" {
+  default = "t2.micro"
 }
